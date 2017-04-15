86155aec53985d096986e51116c6aac39a1caad4
pinkaldas <pinkaldas@gmail.com>
2017-04-15 12:31:36 +0530

    Bootsfaces code

--- work/src/main/java/com/example/pathlabs/navigation/NavigationController.java
+++ work/src/main/java/com/example/pathlabs/navigation/NavigationController.java
@@ -3,10 +3,27 @@
  */
 package com.example.pathlabs.navigation;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
 /**
  * @author WINDOWS
  *
  */
+@ManagedBean
+@ViewScoped
 public class NavigationController {
+	
+	public String moveToHome(){
+		return "home";
+	}
+	
+	public String moveToContact(){
+		return "contacts";
+	}
+	
+	public String moveToAbout(){
+		return "about";
+	}
 
 }
